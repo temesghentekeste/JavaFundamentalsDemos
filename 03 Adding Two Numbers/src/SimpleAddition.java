cb2357460289b7df153c3f63bb46c03cfd514401
temesghentekeste <mail.temesghen.tekeste@gmail.com>
2019-11-06 02:38:42 +0300

    Addition of two numbers

---
+++ 03 Adding Two Numbers/src/SimpleAddition.java
@@ -0,0 +1,9 @@
+public class SimpleAddition {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 200;
+
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
