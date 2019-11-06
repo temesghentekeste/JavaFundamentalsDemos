1af1c0608b942365ac032aece9f9aa6e83eae6d1
temesghentekeste <mail.temesghen.tekeste@gmail.com>
2019-11-06 03:08:02 +0300

    Java Fundamentas Demos

--- 02InputOutput/src/InputOutput.java
+++ 02InputOutput/src/InputOutput.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class InputOutput {
+
     public static void main(String[] args) {
-        System.out.println("Hi there");
+        System.out.println("Welcome to Simple Input and Output Demo Program");
+
+        //Declaring the Scanner Class for getting input from standard input device, keyboard
+        Scanner sc = new Scanner(System.in);
+
+        //String variable to hold the name of a person
+        String name="";
+        //Prompting a person to enter his/her name
+        System.out.print("Enter your name please: ");
+
+        //Accepting name from the keyboard and storing it in the 'name' variable
+        name= sc.next();
+        //Greeting the person
+        System.out.println("Welcome " + name + " to Java Programming!");
+
     }
 }
